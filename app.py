5f88d2aa9ec023bc7c407ee99d9b80fd0dea16bc
AdelNoroozi <adel13no@gmail.com>
2023-03-24 17:02:20 +0330

    -blank routes added (create storage, list storage, retrieve storage, list a storage's cargo & create a cargo)

--- app.py
+++ app.py
@@ -3,4 +3,29 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/storage', methods=['POST'])
+def create_storage():
+    pass
+
+
+@app.route('/storage/<string:storage_name>')
+def retrieve_storage(storage_name):
+    pass
+
+
+@app.route('/storage')
+def get_storage_list():
+    pass
+
+
+@app.route('/storage/<storage_name>/cargos', methods=['POST'])
+def create_cargo(storage_name):
+    pass
+
+
+@app.route('/storage/<storage_name>/cargos')
+def get_cargo_list(storage_name):
+    pass
+
+
 app.run(port=5001)
