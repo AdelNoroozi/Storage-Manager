55eccac082aba7a5b923c523f0a826068a970447
AdelNoroozi <adel13no@gmail.com>
2023-03-26 20:39:16 +0330

    -restful flask initialized (no DB) -retrieving storage -listing storage -creating storage

--- app.py
+++ app.py
@@ -1,74 +1,32 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-storages = [
-    {
-        'name': 'Sattarkhan',
-        'cargos': [
-            {
-                'name': '10kg rice bag',
-                'quantity': 5
-            },
-            {
-                'name': '200g tea box',
-                'quantity': 11
-            }
-        ]
-    },
-    {
-        'name': 'Koshtargah',
-        'cargos': [
-            {
-                'name': 'Gucci Bag',
-                'quantity': 55
-            }
-        ]
-    }
-]
+storages = []
 
 
-@app.route('/storages', methods=['POST'])
-def create_storage():
-    request_data = request.get_json()
-    new_storage = {
-        'name': request_data['name'],
-        'cargos': []
-    }
-    storages.append(new_storage)
-    return jsonify(new_storage)
+class Storage(Resource):
+    def get(self, name):
+        for storage in storages:
+            if storage['name'] == name:
+                return storage, 200
+        return {'storage': None}, 404
+
+    def post(self, name):
+        data = request.get_json()  # by using 'force = True' we can bypass content type header in our request, however it,s dangerous and it is only useful for easier testing
+        storage = {'name': name, 'is_available': data['is_available']}
+        storages.append(storage)
+        return storage, 201
 
 
-@app.route('/storages/<string:storage_name>')
-def retrieve_storage(storage_name):
-    for storage in storages:
-        if storage['name'] == storage_name:
-            return jsonify(storage)
-    return jsonify({'message': 'not found'})
+class StorageList(Resource):
+    def get(self):
+        return {'storages': storages}
 
 
-@app.route('/storages')
-def get_storage_list():
-    return jsonify({'storages': storages})
+api.add_resource(Storage, '/api/storages/<string:name>')
+api.add_resource(StorageList, '/api/storages')
 
-
-@app.route('/storages/<storage_name>/cargos', methods=['POST'])
-def create_cargo(storage_name):
-    request_data = request.get_json()
-    for storage in storages:
-        if storage['name'] == storage_name:
-            new_cargo = {
-                'name': request_data['name'],
-                'quantity': request_data['quantity']
-            }
-            storage['cargos'].append(new_cargo)
-            return jsonify(new_cargo)
-    return jsonify({'message': 'not found'})
-
-
-@app.route('/storages/<storage_name>/cargos')
-def get_cargo_list(storage_name):
-    pass
-
-
-app.run(port=5001)
+app.run(port=5001, debug=True)
