ad41ee295bf63b0023a378759f0f18d6a0a88e03
AdelNoroozi <adel13no@gmail.com>
2023-04-22 22:42:07 +0330

    -posting cargo method added

--- resources/cargo.py
+++ resources/cargo.py
@@ -1,12 +1,30 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 from models.cargo import CargoModel
 
 
 class Cargo(Resource):
+    cargo_parser = reqparse.RequestParser()
+    cargo_parser.add_argument('quantity',
+                              type=int,
+                              required=True,
+                              help="missing field: quantity")
+
     def get(self, name):
         cargo = CargoModel.find_by_name(name)
         if not cargo:
-            return {'message': "cargo not found"}
+            return {'message': "cargo not found"}, 404
         else:
-            return cargo.json()
+            return cargo.json(), 200
+
+    def post(self, name):
+        if CargoModel.find_by_name(name):
+            return {'message': f'cargo with the name {name} already exists'}, 401
+        data = Cargo.cargo_parser.parse_args()
+        quantity = data['quantity']
+        cargo = CargoModel(name=name, quantity=quantity)
+        try:
+            cargo.save()
+        except:
+            return {'message': 'something went wrong'}, 500
+        return cargo.json(), 201
